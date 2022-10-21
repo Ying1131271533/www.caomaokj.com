1a256ae23220bd9ea074381ee58db6745d9e8a66
akali <1131271533@qq.com>
2022-10-21 17:57:54 +0800

    10-21

--- public/index.php
+++ public/index.php
@@ -13,6 +13,10 @@ define('ONE_DAY', 3600 * 24);
 define('ONE_WEEK', 3600 * 24 * 7);
 define('ONE_MONTH', 3600 * 24 * 30);
 
+// 默认应用index
+// define('VIEW_PATH', '');
+// global $view_path;
+
 // 支持事先使用静态方法设置Request对象和Config对象
 // 执行应用并响应
 Container::get('app')->run()->send();
@@ -20,16 +24,26 @@ Container::get('app')->run()->send();
 // 设置视图文件路径
 function viewPath()
 {
-    if (strstr($_SERVER['REQUEST_URI'], 'usezan')) {
-        define('VIEW_PATH', '../application/usezan/view/');
-    } else {
-        // 模版文件路径是PC端还是移动端
-        if (isMobile()) {
-            define('VIEW_PATH', '../application/mobile/view/');
+    if(is_cli()){
+        define('VIEW_PATH', '');
+    } else{
+        if (strstr($_SERVER['REQUEST_URI'], 'usezan')) {
+            define('VIEW_PATH', '../application/usezan/view/');
         } else {
-            define('VIEW_PATH', '../application/index/view/');
+            // 模版文件路径是PC端还是移动端
+            if (isMobile()) {
+                define('VIEW_PATH', '../application/mobile/view/');
+            } else {
+                define('VIEW_PATH', '../application/index/view/');
+            }
         }
     }
+    
+}
+
+// 是否命令行模式
+function is_cli(){
+    return preg_match("/cli/i", php_sapi_name()) ? 1 : 0;
 }
 
 // 判断是手机端浏览还是web端浏览
